089f63a595bca5ff7a1ac5484805cf9de8ba7802
Artem Herasymchuk <artemh@gmail.com>
2014-03-31 12:51:09 -0600

    PEP8 cleanup and fixing bugs

--- iradsanalysis.py
+++ iradsanalysis.py
@@ -1,9 +1,10 @@
 import cherrypy
+from database.database import Database
 from database.mappings import *
 from helpers import *
 from mako.lookup import TemplateLookup
 import datetime
-from Database import database
+
 
 class IradsAnalysis(object):
 
@@ -17,22 +18,22 @@ class IradsAnalysis(object):
         which allows a user to generate a report for analysis,
         based on specified criteria.
         """
-		
-		#Database connection
-		conn = Database()
-		session = Database.get()
-		
+
+        # Database connection
+        conn = Database()
+        session = conn.get()
+
         # Get a list of all patients
-		patients = []
+        patients = []
         for entry in session.query(User).filter(User.class_type == 'p').all():
             if (entry.person.__dict__ not in patients):
                 patients.append(entry.person.__dict__)
-		
+
         template = self.lookup.get_template('analysis/analysis.mako')
         (u, c) = getUserInfo()
-		
-		conn.close()
-		
+
+        conn.close()
+
         return template.render(username=u, classtype=c, patients=patients)
 
     @cherrypy.expose
@@ -40,28 +41,29 @@ class IradsAnalysis(object):
     def generate(self, keywords, options, patient):
         """Returns a generated report for the analysis module"""
         template = self.lookup.get_template('analysis/generate.mako')
-		
-		#Database connection
-		conn = Database()
-		session = Database.get()
-		
-		#Today
-		today = datetime.date.today()
-		
-		#All edge cases are inclusive
-		if options != "all":
-			if options == "week":
-				minimalStartDate = today - datetime.date.today().weekday()
-			elif options == "month":
-				minimalStartDate = today - datetime.date.today().day
-			elif options = "year":
-				minimalStartDate = today - datetime.date.today().timetuple().tm_yday
-		else:
-			results = session.query(RadiologyRecord).filter(
-				RadiologyRecord.test_type.ilike("%" + keywords + "%" ))
-			
-		(u, c) = getUserInfo()
-		
-		conn.close()
-		
+
+        # Database connection
+        conn = Database()
+        session = conn.get()
+
+        # Today
+        today = datetime.date.today()
+
+        # All edge cases are inclusive
+        if options != "all":
+            if options == "week":
+                minimalStartDate = today - datetime.date.today().weekday()
+            elif options == "month":
+                minimalStartDate = today - datetime.date.today().day
+            elif options == "year":
+                minimalStartDate = today - \
+                    datetime.date.today().timetuple().tm_yday
+        else:
+            results = session.query(RadiologyRecord).filter(
+                RadiologyRecord.test_type.ilike("%" + keywords + "%"))
+
+        (u, c) = getUserInfo()
+
+        conn.close()
+
         return template.render(username=u, classtype=c, results=results)
