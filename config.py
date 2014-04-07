59d870e6220e9bc6fbb736966f8857a99b2f8547
Artem Herasymchuk <artemh@gmail.com>
2014-04-07 17:18:27 -0600

    Config

---
+++ config.py
@@ -0,0 +1,8 @@
+#Config File
+
+DATABASE_HOSTNAME = "localhost"
+DATABASE_USERNAME = "irads"
+DATABASE_PASSWORD = "123"
+DATABASE = "irads"
+IP = "0.0.0.0"                              # Listen on all IP addresses
+PORT = 27848
